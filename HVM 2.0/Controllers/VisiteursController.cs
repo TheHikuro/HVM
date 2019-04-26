37b31806129ddcf84c93428a037a74e21905a5b8
Hikuro <loan.cleris@gmail.com>
2019-04-26 15:34:59 +0200

    First try drop down list

--- HVM 2.0/Controllers/VisiteursController.cs
+++ HVM 2.0/Controllers/VisiteursController.cs
@@ -19,12 +19,15 @@ namespace HVM_2._0.Controllers
         public ActionResult Index()
         {
              IList<Creneau> creneauTmpR = new List<Creneau>();
-            CreneauLibre creneauLibre = new CreneauLibre();
-
 
             for (int i = 1; i < 15; i++)
             {
                 creneauTmpR.Add(new Creneau { date = DateTime.Now.AddDays(i) });
+            }
+
+            for(int h = 6; h < 18; h++)
+            {
+                //creneauTmpR.Add(new Creneau { heure = DateTime.Now.AddHours(h) });
             }
 
             foreach(Creneau crn in creneauTmpR)
@@ -39,24 +42,6 @@ namespace HVM_2._0.Controllers
         }
 
 
-            class CreneauLibre
-            {
-                DateTime start;
-                Random gen;
-                int range;
-
-                public CreneauLibre()
-                {
-                    start = new DateTime(1995, 1, 1);
-                    gen = new Random();
-                    range = (DateTime.Today - start).Days;
-                }
-
-                public DateTime Next()
-                {
-                    return start.AddDays(gen.Next(range)).AddHours(gen.Next(0, 24)).AddMinutes(gen.Next(0, 60)).AddSeconds(gen.Next(0, 60));
-                }
-            }
     
         }
     }
