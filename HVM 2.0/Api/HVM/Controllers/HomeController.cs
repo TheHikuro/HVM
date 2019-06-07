dc5aa66bc11f9e32264d25ba084f4aecc812fb19
DESKTOP-0SD0JSA\Loan Cleris <loan.cleris@gmail.com>
2019-06-07 19:55:26 +0200

    final

---
+++ HVM 2.0/Api/HVM/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HVM.Models;
+
+namespace HVM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Database1Entities1 db = new Database1Entities1();
+
+        public ActionResult Index()
+        {
+            System.Console.WriteLine("index loading ");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        { 
+            return View();
+        }
+
+        public int Authentification(String p_Login, String p_password)
+        {
+            // 0 = Erreur lors de la récupération ; 1 = User reconnu ; 2 = Mot de passe ou login incorrect
+            
+            foreach (Patient usr in db.Patient)
+            {
+                if (p_Login == usr.login && p_password == usr.password)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+            return 0;
+        }
+
+        public ActionResult AddToCart(string id)
+        {
+            string newId = id;
+            // return Json(newId, JsonRequestBehavior.AllowGet);
+            var script = @"alert(""Email sent successfully"");";
+            return JavaScript(script);
+        }
+    }
+}
---
+++ HVM 2.0/Api/dbo.Table_1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Creneau]
+(
+	[id_Creneau] INT NOT NULL PRIMARY KEY, 
+    [heure] TIME NULL, 
+    [date] DATE NULL, 
+    [disponibilite] BINARY(50) NULL, 
+    [reserve] BINARY(50) NULL, 
+    [id_Patient] INT NULL, 
+    CONSTRAINT [FK_id_Patient_toPatient] FOREIGN KEY ([id_Patient]) REFERENCES [Patient]([id_Patient])
+)
